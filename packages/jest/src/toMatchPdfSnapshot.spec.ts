a85a94eb489200f4e9951960348bd6a32c2b2772
Benoit TRAVERS <benoit.travers.fr@gmail.com>
2022-09-30 01:17:15 +0200

    refactor: naming

--- packages/jest/src/toMatchPdfSnapshot.spec.ts
+++ packages/jest/src/toMatchPdfSnapshot.spec.ts
@@ -1,5 +1,5 @@
-import path from 'node:path';
-import fs from 'node:fs';
+import * as path from 'node:path';
+import * as fs from 'node:fs';
 import { GenericContainer, StartedTestContainer } from 'testcontainers';
 
 describe('Jest extension', () => {
